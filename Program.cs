ca09bc0d70f9e3d81be1412f51b940933d278361
Jill Oakes <jericho_rules@hotmail.com>
2022-04-01 11:21:07 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isValidTemp;
+            double result;
+
+            Console.WriteLine("Welcome to the Temperature Converter.");
+            
+            do
+            {
+                Console.Write("Enter the temperature in Fahrenheit that you wish to convert: ");
+                isValidTemp = double.TryParse(Console.ReadLine(), out result);
+            }while (!isValidTemp);
+
+            Console.WriteLine($"{result} degrees Fahrenheit is the same as {TempConverter.FahrenheitToCelsius(result)} degrees Celsius.");
+            
+            Console.WriteLine("----------");
+            
+            bool isValidTemp2;
+            double result2;
+            do
+            {
+                Console.Write("Enter the temperature in Celsius that you wish to convert: ");
+                isValidTemp2 = double.TryParse(Console.ReadLine(), out result2);
+            } while (!isValidTemp2);
+
+            Console.WriteLine($"{result2} degrees Celsius is the same as {TempConverter.CelsiusToFahrenheit(result2)} degrees Fahrenheit.");
+        }
+    }
+}
---
+++ TempConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double temp)
+        {
+            return (temp - 32) * 5 / 9;
+        }
+
+        public static double CelsiusToFahrenheit(double temp)
+        {
+            return (temp * 9 / 5) + 32;
+        }
+    }
+}
